fa425b98ef9c7ff3f3bd8dda960ee71fc80865c1
xh2513@columbia.edu <bobby@BobbyFichersMBP.lan>
2022-04-07 00:01:25 -0400

    Add libpng installation script

--- install.sh
+++ install.sh
@@ -3,6 +3,19 @@ echo "Installing all necessary dependencies (open install.sh for more detail)"
 echo "Unzipping:\n\t $base_dir/lib/jpegsrc.v9e.tar.gz"
 echo "*****"
 
+tar -xzf "$base_dir/lib/libpng-1.6.37.tar.xz" -C "$base_dir/lib/"
+
+echo "Installing libpng library"
+echo "*****"
+cd "$base_dir/lib/libpng-1.6.37"
+./configure --prefix="$base_dir/lib/"
+make test
+make
+make install
+cd "$base_dir"
+rm -rf "$base_dir/lib/libpng-1.6.37"
+
+
 tar -xzf "$base_dir/lib/jpegsrc.v9e.tar.gz" -C "$base_dir/lib/"
 
 echo "Installing jpeg-9e library"
@@ -34,7 +47,7 @@ echo "\n\nUnzipping:\n\t $base_dir/lib/boost_1_77_0.zip "
 echo "*****"
 cd "$base_dir/lib"
 echo "Please wait this will take a few minutes"
-unzip -q -n boost_1_77_0.zip 
+unzip -q -n boost_1_77_0.zip
 echo "Unzip complete"
 
 cd "$base_dir"
