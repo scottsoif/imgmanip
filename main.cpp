0e18d15a1a5b6e144c106e0453f17605e92ffc08
Scott Soifer <soifer00@gmail.com>
2022-04-12 08:58:51 -0400

    Added ratio_crop function Debugged regular crop functino by fixing offsets

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "./imgmanip/imgio/imgio.h"
 #include "./imgmanip/homography.h"
+#include "./imgmanip/mosaic.h"
 
 
 using namespace std;
@@ -23,10 +24,10 @@ void testHomography(Cube<int> srcImg){
                        {0, right_idx}, //top- right
                        { bottom_idx, right_idx }, //bottom-right
                        {bottom_idx, 0}}; // bottom-left
-  mat destination = {{50,5}, 
-  {0, right_idx-50},
-  { bottom_idx,  right_idx },
-  {bottom_idx, 0}};
+  mat destination = {{0,50}, 
+  {50, right_idx+0},
+  { bottom_idx+0,  right_idx-50 },
+  {bottom_idx-50, 0+0}};
 
   mat H_3x3 = computeHomography(startPoints, destination);
   Cube<int> newImg = genHomographyImgCanvas<int>(srcImg, H_3x3);
@@ -36,16 +37,32 @@ void testHomography(Cube<int> srcImg){
 
 }
 
+void testMosiac(){
+
+  Cube<int> srcImg = read_img<int>("imgs/tiles/IMG_9738.png");
+
+// demonstrating that the h or w can be greater than img size
+  Cube<int> croppedImg = crop<int>(srcImg, 1800, 600, 7000, 400);
+  write_img(croppedImg, "imgs/tiles/simple_cropped_IMG_9738.png");
+
+  // Cube<int> ratioCroppedImg = max_crop<int>(srcImg, 50/120.0);
+  // write_img(ratioCroppedImg, "imgs/tiles/ratio_cropped_IMG_9738.png");
+
+  cout << "saved imgs to directory imgs/tiles/" << endl;
+
+
+}
+
+
 int main(int argc, char const *argv[])
 {
   /* code */
-  imgio_hello_word();
+  // imgio_hello_word();
   Cube<int> img = read_img<int>("test.jpg");
-
-
-  
-  //cout << img(0,0,0) <<endl;
   testHomography(img);
+
+  testMosiac();
+
   write_img(img, "test3.jpeg");
   return 0;
 }
